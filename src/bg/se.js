c7804fda943f347fb4991c971e6f5c94194bde8b
A. Taha Baki <54471918+atahabaki@users.noreply.github.com>
2021-08-08 02:18:20 +0300

    ✨ SE class created...

---
+++ src/bg/se.js
@@ -0,0 +1,30 @@
+/*
+ *  This file is part of Smart Search.
+ *
+ *  Smart Search is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Smart Search is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Smart Search.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
+ * SE: Abbreviation of "Search Engine"
+ * 
+ * Holds a codename, actual name, and the url...
+ *
+*/
+class SE {
+	constructor(codename, name, url) {
+		this.codename = codename
+		this.name = name
+		this.url = url
+	}
+}
