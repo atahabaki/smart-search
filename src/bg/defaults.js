1649bb489f640c49218e3894ad731c9b4f91ae97
A. Taha Baki <54471918+atahabaki@users.noreply.github.com>
2021-08-08 15:00:51 +0300

    ✨ fallback SEs defined...

---
+++ src/bg/defaults.js
@@ -0,0 +1,36 @@
+/*
+ *  Smart Search - A web browser extension which is eases the way you search.
+ *  Copyright (C) 2021 A. Taha Baki
+ *
+ *  This file is part of Smart Search.
+ *
+ *  Smart Search is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Smart Search is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Smart Search.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/**
+ * Before loading the search engines with 'browser.storage' API, 
+ * use these SEs as default.
+ * @type {Array.<SE>}
+ */
+const FALLBACK_SEARCH_ENGINES = [
+	new SE("ytb", "YouTube", "https://www.youtube.com/results?search_query={%query%}"),
+	new SE("ggl", "Google", "https://www.google.com/search?q={%query%}"),
+	new SE("bng", "Bing", "https://www.bing.com/search?q={%query%}"),
+	new SE("yaho", "Yahoo", "https://search.yahoo.com/?q={%query%}"),
+	new SE("dckg", "DuckDuckGo", "https://duckduckgo.com/?q={%query%}"),
+	new SE("iconf", "IconFinder", "https://www.iconfinder.com/search/?q={%query%}"),
+	new SE("rddt", "Reddit", "https://www.reddit.com/search/?q={%query%}"),
+	new SE("gthb", "GitHub", "https://github.com/search?q={%query%}"),
+	new SE("adox", "Android Developers", "https://developer.android.com/s/results?q={%query%}"),
+]
